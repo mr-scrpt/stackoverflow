a8735b5d78080b778807eeb23c8982beadc65369
mr.scrpt <mr.scrpt@gmail.com>
2023-11-08 13:02:56 +0200

    test

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -10,12 +10,12 @@ export default function Home() {
     <section>
       <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
         <h1 className="h1-bold text-dark100_light900">All Questions</h1>
-
         <Link href="/ask-question" className="flex justify-end max-sm:w-full">
           <Button className="primary-gradient min-h-[40px] px-4 py-3 text-light-900">
             Ask question
           </Button>
         </Link>
+        text
       </div>
 
       <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
