8862c44eddfdd33bb90e64020657be125c81f753
mr.scrpt <mr.scrpt@gmail.com>
2023-11-09 11:01:57 +0200

    implemented: base form question without tinymce editor

--- components/shared/QuestionForm/QuestionForm.tsx
+++ components/shared/QuestionForm/QuestionForm.tsx
@@ -33,23 +33,78 @@ export const QuestionForm: FC<QuestionFormProps> = (props) => {
   }
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="flex w-full flex-col gap-10"
+      >
         <FormField
           control={form.control}
           name="title"
           render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
+            <FormItem className="flex w-full flex-col gap-2">
+              <FormLabel className="paragraph-semibold text-dark400_light800 font-bold">
+                Question Title <span className="text-primary-500">*</span>
+              </FormLabel>
               <FormControl>
-                <Input placeholder="shadcn" {...field} />
+                <Input
+                  placeholder="shadcn"
+                  {...field}
+                  className="mt-0 no-focus paragraph-regular bg-light900_dark300 light-border-2 text-dark300_light700 min-h-[56px] border"
+                />
               </FormControl>
-              <FormDescription>
-                This is your public display name.
+              <FormDescription className="mt-0 text-[0.8rem] dark:text-slate-400 body-regular text-light-500">
+                Be specific and imagine you're asking a question to another
+                person.
               </FormDescription>
-              <FormMessage />
+              <FormMessage className="text-[0.8rem] text-red-500" />
             </FormItem>
           )}
         />
+
+        <FormField
+          control={form.control}
+          name="explanation"
+          render={({ field }) => (
+            <FormItem className="flex w-full flex-col gap-2">
+              <FormLabel className="paragraph-semibold text-dark400_light800 font-bold">
+                Detail explanation of your question?
+                <span className="text-primary-500">*</span>
+              </FormLabel>
+              <FormControl>Editor</FormControl>
+
+              <FormMessage className="text-[0.8rem] text-red-500" />
+              <FormDescription>
+                Introduce the problem and expand on what you put in the title.
+                Minimum 20 characters.
+              </FormDescription>
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="tags"
+          render={({ field }) => (
+            <FormItem className="flex w-full flex-col gap-2">
+              <FormLabel className="paragraph-semibold text-dark400_light800 font-bold">
+                Tags <span className="text-primary-500">*</span>
+              </FormLabel>
+              <FormControl>
+                <Input
+                  {...field}
+                  className="mt-0 no-focus paragraph-regular bg-light900_dark300 light-border-2 text-dark300_light700 min-h-[56px] border"
+                  placeholder="Tags..."
+                />
+              </FormControl>
+              <FormDescription className="mt-0 text-[0.8rem] dark:text-slate-400 body-regular text-light-500">
+                Add up to 3 tags to describe what your question is about. You
+                need to press enter to add a tag.
+              </FormDescription>
+              <FormMessage className="text-[0.8rem] text-red-500" />
+            </FormItem>
+          )}
+        />
+
         <Button type="submit">Submit</Button>
       </form>
     </Form>
--- components/shared/QuestionForm/validation.schema.ts
+++ components/shared/QuestionForm/validation.schema.ts
@@ -2,4 +2,6 @@ import * as z from 'zod'
 
 export const QuestionFormSchema = z.object({
   title: z.string().min(5).max(130),
+  explanation: z.string().min(100).max(15000),
+  tags: z.array(z.string().min(1).max(15)).min(1).max(3),
 })
