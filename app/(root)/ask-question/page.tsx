76493dbd8ff079ec7f83dd5d6adfb6e8a66ccb06
mr.scrpt <mr.scrpt@gmail.com>
2023-11-09 09:57:26 +0200

    create: base question form component

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,3 +1,25 @@
-export default function AskPage() {
-  return <div>Ask Page</div>
+import { QuestionForm } from '@/components/shared/QuestionForm/QuestionForm'
+import { redirect } from 'next/navigation'
+import { FC, HTMLAttributes } from 'react'
+
+interface PageProps extends HTMLAttributes<HTMLDivElement> {}
+
+const Page: FC<PageProps> = (props) => {
+  const userId = '123456'
+
+  if (!userId) redirect('/sign-in')
+
+  // const mongoUser = await getUserById({ userId })
+
+  return (
+    <section className="flex flex-col gap-8">
+      <h1 className="h1-bold text-dark100-light900">Ask a Question</h1>
+
+      <div className="flex flex-col">
+        <QuestionForm />
+      </div>
+    </section>
+  )
 }
+
+export default Page
---
+++ components/shared/QuestionForm/QuestionForm.tsx
@@ -0,0 +1,7 @@
+import { FC, HTMLAttributes } from 'react'
+
+interface QuestionFormProps extends HTMLAttributes<HTMLDivElement> {}
+
+export const QuestionForm: FC<QuestionFormProps> = (props) => {
+  return <div>QuestionForm</div>
+}
